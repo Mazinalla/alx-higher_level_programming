081fa035facb1c5ccb17350e2069d7a8645d2fa4
mazinalla <mazinelsadig8@gmail.com>
2024-01-11 19:48:57 -0800

    py

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for letter in range(97, 101):
+    print(f"{chr(letter)}", end="")
+for let in range(102, 113):
+    print(f"{chr(let)}", end="")
+for lett in range(114, 123):
+    print(f"{chr(lett)}", end="")
