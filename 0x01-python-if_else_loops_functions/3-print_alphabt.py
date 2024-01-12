903b3f70c7b134e7840ee71dd6be90deda5945b4
mazinalla <mazinelsadig8@gmail.com>
2024-01-11 20:20:01 -0800

    py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for letter in range(97, 101):
-    print(f"{chr(letter)}", end="")
-for let in range(102, 113):
-    print(f"{chr(let)}", end="")
-for lett in range(114, 123):
-    print(f"{chr(lett)}", end="")
+for letter in range(97, 123):
+    if chr(letter) != 'e' and chr(letter) != 'q':
+        print("{}".format(chr(letter)), end="")
