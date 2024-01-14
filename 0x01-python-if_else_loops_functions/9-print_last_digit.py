b12d861c8d3421441a489ce061714f11f45e484b
mazinalla <mazinelsadig8@gmail.com>
2024-01-13 21:25:25 -0800

    py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number > 10:
-        print("{}".format(number % 10))
-    else:
-        print("{:1d}".format(number))
+    print("{}".format(abs(number) % 10), end="")
+    return(abs(number) % 10)
