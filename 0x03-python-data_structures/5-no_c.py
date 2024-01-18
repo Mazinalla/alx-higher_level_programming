2022104957ef0fba25a7fa7d50bdea8555c4492c
mazinalla <mazinelsadig8@gmail.com>
2024-01-17 16:54:17 -0800

    py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = ""
     for i in range(len(my_string)):
         if my_string[i] != "c" and my_string[i] != "C":
-            print(my_string[i], end="")
+            new_string += my_string[i]
         else:
             pass
-    print("")
-    return my_string
+    return new_string
