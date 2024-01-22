661866eb2a2397dcb45cce8ff0feb4d01a7ca020
mazinalla <mazinelsadig8@gmail.com>
2024-01-22 15:07:43 -0800

    py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+my_list = [1, 2, 3, 4, 5]
+def safe_print_list(my_list=[], x=0):
+    my_list = [1, 2, 3, 4, 5]
+    if x <= 0:
+        pass
+    elif x > 0 and x <= len(my_list):
+        for i in range(len(my_list)):
+            print(my_list[i], end="")
+            if i == x - 1:
+                break
+        print()
+    else:
+        for i in range(len(my_list)):
+            print(my_list[i], end="")
+        print()    
+        x = len(my_list)
+    return x
