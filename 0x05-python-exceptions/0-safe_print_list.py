6c54524ae0c8a0dd87c88a09d3a32a3bcc5a8dba
mazinalla <mazinelsadig8@gmail.com>
2024-01-23 03:00:03 -0800

    py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,19 +2,23 @@
 my_list = [1, 2, 3, 4, 5]
 def safe_print_list(my_list=[], x=0):
     my_list = [1, 2, 3, 4, 5]
+    length = 0
+    for a in my_list:
+        length += 1
     if x <= 0:
         pass
-    elif x > 0 and x <= len(my_list):
-        for i in range(len(my_list)):
+    elif x > 0 and x <= length:
+        for i in range(length):
             try:
                 print(my_list[i], end="")
-            except:
                 if i == x - 1:
                     break
+            except:
+                print("invalid")
         print()
     else:
-        for i in range(len(my_list)):
+        for i in range(length):
             print(my_list[i], end="")
         print()    
-        x = len(my_list)
+        x = length
     return x
