7957b0ab6b6312ee40fe2f3a870066d1705f2123
mazinalla <mazinelsadig8@gmail.com>
2024-01-23 03:03:02 -0800

    py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -14,7 +14,7 @@ def safe_print_list(my_list=[], x=0):
                 if i == x - 1:
                     break
             except:
-                print("invalid")
+                break
         print()
     else:
         for i in range(length):
