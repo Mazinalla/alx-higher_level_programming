34313b221776ebb039c3b99fcf81e91472ca4792
mazinalla <mazinelsadig8@gmail.com>
2024-02-02 02:34:37 -0800

    py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in my_list:
-        if idx < 1:
-            return None
-        elif idx > len(my_list):
-            return None
-        else:
-            return my_list[idx]
+    if idx < 1:
+        return None
+    elif idx > len(my_list):
+        return None
+    else:
+        for i in my_list:
+            if my_list[i] == idx + 1:
+                return my_list[i]
