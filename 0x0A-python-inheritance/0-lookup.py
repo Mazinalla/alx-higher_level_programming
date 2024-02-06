804c887710a4ea4c2e320b1eadbd1e0f92dc0a56
mazinalla <mazinelsadig8@gmail.com>
2024-02-05 18:31:16 -0800

    py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def lookup(obj):
-    attributes_and_methods = dir(obj)
-    result = [attr for attr in attributes_and_methods if not attr.startswith('__')]
-    return result
+    return dir(obj)
