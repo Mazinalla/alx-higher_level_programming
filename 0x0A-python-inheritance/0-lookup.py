31113ea6e27dafe59bc1c8057f8864ae2592d40a
mazinalla <mazinelsadig8@gmail.com>
2024-02-05 19:59:52 -0800

    py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+
+"""function that returns the list of available attributes and methods of an object """
+
+
 def lookup(obj):
+    """built in function returns all attributes and methods """
     return dir(obj)
