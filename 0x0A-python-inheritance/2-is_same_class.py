2ab928a62d3869911d7ec481be00f8b700b57349
mazinalla <mazinelsadig8@gmail.com>
2024-02-06 04:57:45 -0800

    py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,6 +3,9 @@
 '''function that returns True if the object is exactly an instance of the specified class ; otherwise False'''
 
 def is_same_class(obj, a_class):
+
+    '''compare between the method and attribute of the 2 dir'''
+
     if dir(obj) == dir(a_class):
         return True
     else:
