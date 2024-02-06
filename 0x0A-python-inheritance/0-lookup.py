e26511c45995d13f63328886263514443eb5ac3e
mazinalla <mazinelsadig8@gmail.com>
2024-02-05 18:23:55 -0800

    py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def lookup(obj):
-    # Use dir() to get the list of attributes and methods
     attributes_and_methods = dir(obj)
-
-    # Filter out private and special methods (those starting with '__')
     result = [attr for attr in attributes_and_methods if not attr.startswith('__')]
-
     return result
