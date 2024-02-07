7d46b2ef9dfdaecfabbb42b88c1aa0e03662b1d6
mazinalla <mazinelsadig8@gmail.com>
2024-02-06 18:05:44 -0800

    py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,7 +5,7 @@
 def write_file(filename="", text=""):
 
     '''function that write on text file'''
-    with open(filename, mode="w", encoding="UTF8") as file
+    with open(filename, mode="w", encoding="utf-8") as file
     num_characters_written = file.write(text)
 
     '''function that return length of the text'''
