0db11ca8d252cca0d654231b088769abb676a5a3
mazinalla <mazinelsadig8@gmail.com>
2024-02-16 03:35:35 +0300

    py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,9 +3,9 @@ for i in range(97, 123):
     continue
 while(i >= 97):
     if i % 2 == 0:
-        print(chr(i), end="")
+        print("{}".format(chr(i)), end="")
         i -= 1
     else:
-        print(chr(i - 32), end="")
+        print("{}".format(chr(i - 32)), end="")
         i -= 1
 
