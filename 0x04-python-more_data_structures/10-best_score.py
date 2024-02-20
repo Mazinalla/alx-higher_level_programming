6e81ac751cc28775c89335c06e69747153c21783
mazinalla <mazinelsadig8@gmail.com>
2024-02-20 19:05:45 +0300

    py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,6 +6,10 @@ def best_score(a_dictionary):
         return None
 
     # Find the key with the maximum value
-    max_key = max(a_dictionary, key=a_dictionary.get)
+    max_key = max(a_dictionary)
     
     return max_key
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
