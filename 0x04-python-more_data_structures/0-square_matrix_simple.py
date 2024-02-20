8e756955913939eda38c09adcff2c1176e6700bd
mazinalla <mazinelsadig8@gmail.com>
2024-02-20 08:13:29 +0300

    py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -12,3 +12,13 @@ def square_matrix_simple(matrix=[]):
         return list(map(lambda x: x ** 2, row))
 
     return list(map(square_row, matrix))
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
