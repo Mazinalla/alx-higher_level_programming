965189063ae84ebabee8a225c13620f29399f91a
mazinalla <mazinelsadig8@gmail.com>
2024-02-20 14:33:04 +0300

    py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -8,10 +8,3 @@ def simple_delete(a_dictionary, key=""):
         return a_dictionary
     else:
         return a_dictionary
-
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
-new_dict = simple_delete(a_dictionary, 'c_is_fun')
-print(new_dict)
-
-new_dict = simple_delete(a_dictionary, 'track')
-print(new_dict)
