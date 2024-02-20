2753eb2359d7e60cf6e27f2a3121826de77c0b28
mazinalla <mazinelsadig8@gmail.com>
2024-02-20 08:53:11 +0300

    py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
 def search_replace(my_list, search, replace):
     '''Create a new list to store the modified elements'''
     new_list = list(map(lambda x: replace if x == search else x, my_list))
