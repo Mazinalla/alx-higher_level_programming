4174872363c49dc71c8b71bba292bd6a9f4fe82c
mazinalla <mazinelsadig8@gmail.com>
2024-02-20 10:37:40 +0300

    py

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    '''a function that returns a set of all elements present in only one set'''
+    result = []
+    for ele in set_1:
+        if ele not in set_2:
+            result.append(ele)
+    for ele in set_2:
+        if ele not in set_1:
+            result.append(ele)
+    return result
