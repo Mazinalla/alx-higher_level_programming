8def44cf0f5476680a9b9ed2e8047105719377f6
mazinalla <mazinelsadig8@gmail.com>
2024-02-20 11:53:48 +0300

    py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,19 +2,7 @@
 
 def print_sorted_dictionary(a_dictionary):
     '''Get the sorted list of keys'''
-    sorted_keys = sorted(a_dictionary.keys())
-
-    # Iterate through sorted keys and print key-value pairs
-    for key in sorted_keys:
-        value = a_dictionary[key]
-
-        # Check if the value is a nested dictionary
-        if isinstance(value, dict):
-            print(f"{key}: ", end="")
-            print_sorted_dictionary(value)
-        else:
-            print(f"{key}: {value}")
-
-
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
-print_sorted_dictionary(a_dictionary)
+    list_ord = list(a_dictionary.keys())
+    list_ord.sort()
+    for i in list_ord:
+        print("{}: {}".format(i, a_dictionary.get(i)))
