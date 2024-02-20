fa76c038b954c55726cb3735bcc2371e74c4ef7a
mazinalla <mazinelsadig8@gmail.com>
2024-02-20 15:02:20 +0300

    py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        a_dictionary[key] **= 2
-    return a_dictionary
+    for key, value in a_dictionary.items():
+        value **= 2
+        print(key, ":", value)
+    
+
