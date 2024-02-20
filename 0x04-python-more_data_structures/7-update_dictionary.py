086f7c26533f704f0a9517223da22bdc01722f94
mazinalla <mazinelsadig8@gmail.com>
2024-02-20 14:16:12 +0300

    py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,12 +2,5 @@
 
 def update_dictionary(a_dictionary, key, value):
     ''' a function that replaces or adds key/value in a dictionary'''
-    lista = list(a_dictionary)
-    for i in lista:
-        if i == key:
-            continue
-        else:
-            lista.append(key)
-    dicta = dict(lista)
-    dicta[key] = value
-    return dicta
+    a_dictionary[key] = value
+    return a_dictionary
