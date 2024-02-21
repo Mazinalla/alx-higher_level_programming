1737122760aabda5be9f61c45db5fa09379343c5
mazinalla <mazinelsadig8@gmail.com>
2024-02-21 17:21:33 +0300

    py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+    - __size (int): The length of each side of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a Square instance with an optional specified side length.
+
+        Parameters:
+        - size (int, optional): The length of each side of the square. Default is 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
