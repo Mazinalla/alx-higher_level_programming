2c78ec1df38bce62bcfa436329b940bbb2f86ac7
mazinalla <mazinelsadig8@gmail.com>
2024-02-21 15:53:19 +0300

    py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''class that defines a square'''
+
+class Square:
+
+    '''
+    class that defines a square
+    attr: size
+    methode: none
+    '''
+    def __init__(self, __size):
+        self.size = __size
