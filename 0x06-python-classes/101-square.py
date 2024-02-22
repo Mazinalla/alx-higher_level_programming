27f6514c8a4405b2428fbb8c27a2ad74b36856e4
mazinalla <mazinelsadig8@gmail.com>
2024-02-22 07:54:09 +0300

    py

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,82 +1,105 @@
 #!/usr/bin/python3
 
 '''
-class that defines a square
+Define a class Square.
 '''
 
 class Square:
-    '''
-    class that defines a square
-    '''
+    """
+    Square class represents a square with a size and position.
+
+    Attributes:
+    - size (int): The size of the square.
+    - position (tuple): The position of the square as a tuple of two positive integers.
+    """
 
     def __init__(self, size=0, position=(0, 0)):
-        '''
-        Initializes a square instance with specified size and position
+        """
+        Initializes a new instance of the Square class.
 
-        parameter:
-        - size
-        - position
-        '''
+        Parameters:
+        - size (int): The size of the square (default is 0).
+        - position (tuple): The position of the square (default is (0, 0)).
+        """
         self.size = size
         self.position = position
 
-
     @property
     def size(self):
-        '''
-        Getter method for the value attribute.
+        """
+        Getter method for the size attribute.
 
-        return:
-        The reference to the attribute
-        '''
+        Returns:
+        - int: The size of the square.
+        """
         return self._size
-    
+
     @size.setter
     def size(self, value):
-        '''
-        Setter method for the value attribute.
+        """
+        Setter method for the size attribute.
+
         Parameters:
-        - value
-        '''
+        - value (int): The size to set.
+
+        Raises:
+        - TypeError: If size is not an integer.
+        - ValueError: If size is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self._size = value
+        self._size = value
 
-    @property        
+    @property
     def position(self):
-        '''
-        getter method for the position attr.
+        """
+        Getter method for the position attribute.
 
-        return:
-        - 
-        '''
+        Returns:
+        - tuple: The position of the square.
+        """
         return self._position
 
     @position.setter
     def position(self, value):
-        '''
-        setter method for the position attr.
-        
-        para:
-        - value
-        ''' 
-        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(i, int) for i in value) or any(i < 0 for i in value):
+        """
+        Setter method for the position attribute.
+
+        Parameters:
+        - value (tuple): The position to set.
+
+        Raises:
+        - TypeError: If position is not a tuple of two positive integers.
+        """
+        if (
+            not isinstance(value, tuple) or
+            len(value) != 2 or
+            not all(isinstance(i, int) for i in value) or
+            any(i <= 0 for i in value)
+        ):
             raise TypeError("position must be a tuple of 2 positive integers")
         self._position = value
-    
+
     def area(self):
-        '''
-        puplic method that return the current square area
-        '''
+        """
+        Calculates the area of the square.
+
+        Returns:
+        - int: The area of the square.
+        """
         return self.size ** 2
-    
+
     def my_print(self):
-        '''
-        puplic method that prints in stdout the square with the character #.
-        '''
+        """
+        Prints the square with the character '#' in stdout.
+
+        If size is equal to 0, prints an empty line.
+        Uses the position to adjust the output by using space.
+
+        Printing a Square instance should have the same behavior as my_print().
+        """
         if self.size == 0:
             print()
             return
@@ -86,11 +109,3 @@ class Square:
 
         for _ in range(self.size):
             print(" " * self.position[0] + "#" * self.size)
-
-my_square = Square(5, (0, 0))
-print(my_square)
-
-print("--")
-
-my_square = Square(5, (4, 1))
-print(my_square)
