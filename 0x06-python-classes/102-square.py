60d3271455aebe8e9e6deecb39a4c562abab93f9
mazinalla <mazinelsadig8@gmail.com>
2024-02-22 08:23:16 +0300

    py

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,50 +1,48 @@
 #!/usr/bin/python3
+""" class Square that defines a square"""
 
-'''class define square'''
 
 class Square:
-    '''
-    A class representing a square.
-
-    Attributes:
-    - __size (int): The length of each side of the square.
-    '''
+    """ class Square that defines a square"""
     def __init__(self, size=0):
-        '''
-        Initializes a Square instance with an optional specified side length.
+        """ init square
 
-        Parameters:
-        - size (int, optional): The length of each side of the square. Default is 0.
-        '''
+        Args:
+            value (int): size of the square.
+        """
         self.size = size
 
     @property
     def size(self):
-        '''
-        property to retrieve size
-        '''
+        """int: private size.
+
+        Returns:
+            Private size.
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        '''
-        property to set the value
+        """Sets value into size, must be int.
 
-        para:
-        - value of the size
-        '''
-        if not isinstance(value, int,float):
-            raise TypeError("size must be a number")
+        Args:
+            value (int): size of the square.
+        """
+        if type(value) is not int and type(value) is not float:
+            raise TypeError('size must be a number')
         elif value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         else:
-            self.__size = value
-    
+            self.__size = value  #: size of the square
+
     def area(self):
-        '''
-        puplic method that returns the current square area
-        '''
-        return self.__size ** 2
+        """returns the area
+
+        Returns:
+            area.
+        """
+        return self.__size**2
+
     def __lt__(self, other):
         return self.size < other.size
 
