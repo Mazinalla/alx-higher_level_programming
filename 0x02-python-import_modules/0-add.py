cc3ee9638045132f00c838132e18cae94de0a843
mazinalla <mazinelsadig8@gmail.com>
2024-02-26 02:29:52 +0300

    py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     from add_0 import add
     
@@ -6,3 +7,4 @@ if __name__ == "__main__":
     b = 2
     
     print("{0} + {1} = {2}".format(a, b, add(a, b)))
+
