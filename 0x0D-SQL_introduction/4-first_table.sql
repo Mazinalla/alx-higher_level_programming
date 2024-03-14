e5e5f46ea90e97883b10803f43646c13742e3dd6
mazinalla <mazinelsadig8@gmail.com>
2024-03-15 00:06:36 +0300

    SQL

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,5 @@
 -- create the table first_table in the current DATABASE in mysql server
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR);
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256)
+    );
