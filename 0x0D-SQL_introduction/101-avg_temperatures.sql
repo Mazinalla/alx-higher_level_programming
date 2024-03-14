364cdeff778f2a97e12ace11a6d55e9a282d7a58
mazinalla <mazinelsadig8@gmail.com>
2024-03-15 02:47:11 +0300

    SQL

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,5 @@
 -- displays the average temperature (Fahrenheit) by city ordered by temperature (descending) :-
+mysql -h localhost -u root -p hbtn_0c_0 < temperatures.sql
 SELECT city, AVG(temperature * 9/5 + 32) AS avg_temp_fahrenheit
 FROM weather_data
 GROUP BY city
