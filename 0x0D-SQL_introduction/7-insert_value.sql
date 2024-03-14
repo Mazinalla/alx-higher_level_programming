764ea47ad79ce02489207d10ca02623d563e7086
mazinalla <mazinelsadig8@gmail.com>
2024-03-15 01:47:57 +0300

    SQL

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,9 @@
+-- Insert new row in the table first_table in MySQL server :-
+INSERT INTO (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256)
+)
+VALUE (
+    89,
+    "Best School"
+);
