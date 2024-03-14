60163e8889127a1aaae7fc0ee50742bbf85c86d6
mazinalla <mazinelsadig8@gmail.com>
2024-03-15 01:51:41 +0300

    SQL

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,9 +1,2 @@
 -- Insert new row in the table first_table in MySQL server :-
-INSERT INTO first_table(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256)
-)
-VALUE (
-    89,
-    "Best School"
-);
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
