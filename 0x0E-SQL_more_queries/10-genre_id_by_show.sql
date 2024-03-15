fe366cae90294a02b32d9c4ef40038e9bef5bdba
mazinalla <mazinelsadig8@gmail.com>
2024-03-15 15:17:01 +0300

    SQL

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked :-
-source C:\xampp\mysql\bin\hbtn_0d_tvshows.sql.txt
+mysql < C:\xampp\mysql\bin\hbtn_0d_tvshows.sql.txt
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
