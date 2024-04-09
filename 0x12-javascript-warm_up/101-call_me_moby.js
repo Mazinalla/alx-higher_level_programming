44243260192851db01d91a8a053007a6e7f38ac0
mazinalla <mazinelsadig8@gmail.com>
2024-04-09 21:59:28 +0300

    Js

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-function executeXTimes(x, theFunction) {
-  for (let i = 0; i < x; i++) {
-    theFunction();
+exports.callMeMoby = function (x, theFunction) {
+  while (x > 0) {
+    theFunction.call();
+    x--;
   }
-}
+};
