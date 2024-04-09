18ef19f1f799bce7aa69820f065fa73f3d47cc4f
mazinalla <mazinelsadig8@gmail.com>
2024-04-08 19:55:14 -0700

    Js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 const firstArgument = process.argv[2];
 const secondArgument = process.argv[3];
-if (firstArgument !== undefined && secondArgument !== undefined) {
-  console.log(`${firstArgument} is ${secondArgument}`);
-} else {
-  console.log('Please provide two arguments.');
-}
+console.log(`${firstArgument} is ${secondArgument}`);
