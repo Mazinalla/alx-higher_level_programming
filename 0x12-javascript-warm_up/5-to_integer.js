27407e74d19d891dffa051810a8a22102c88a0f1
mazinalla <mazinelsadig8@gmail.com>
2024-04-09 05:57:07 +0300

    Js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const firstArgument = process.argv[2];
+const integerValue = +firstArgument;
+if (!isNaN(integerValue)) {
+    console.log(`My number: ${integerValue}`);
+} else {
+    console.log("Not a number");
+}
