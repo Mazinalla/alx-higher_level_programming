9f73a472e9479169e3f33630db7e28500b958ae6
mazinalla <mazinelsadig8@gmail.com>
2024-04-08 19:48:42 -0700

    Js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const firstarg = process.argv[1];
-if(firstarg === 0){
-    console.log('No argument');
-}else{
-    console.log(firstarg);
+const firstarg = process.argv[2];
+if (firstarg === undefined) {
+  console.log('No argument');
+} else {
+  console.log(firstarg);
 }
