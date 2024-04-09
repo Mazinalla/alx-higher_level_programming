944a9ce1863c08e7b0bf72e10bc4ddd9581c9067
mazinalla <mazinelsadig8@gmail.com>
2024-04-08 20:08:43 -0700

    Js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,8 +3,8 @@ const numOccurrences = process.argv[2];
 const integerValue = +numOccurrences;
 if (!isNaN(integerValue)) {
   for (let i = 0; i < integerValue; i++) {
-      console.log('C is fun');
-    }
+    console.log('C is fun');
+  }
 } else {
   console.log('Missing number of occurrences');
 }
