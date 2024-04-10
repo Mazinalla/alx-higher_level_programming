5a6f795f8e2a9efa02d93262ad4188b133a3719b
mazinalla <mazinelsadig8@gmail.com>
2024-04-10 03:10:55 +0300

    Js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,8 +5,12 @@ class Square extends square {
     if (c === undefined) {
       c = 'X';
     }
-    for (let i = 0; i < this.size; i++) {
-      console.log(c.repeat(this.size));
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
     }
   }
 }
