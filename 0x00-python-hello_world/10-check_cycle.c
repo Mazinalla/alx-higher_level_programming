5b2abe8722a930a8e223d9fed9f4176787132278
mazinalla <mazinelsadig8@gmail.com>
2024-04-22 00:55:25 +0300

    Linked list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,12 +3,14 @@
 #include "lists.h"
 
 int check_cycle(listint_t *list) {
+    listint_t *slow;
+    listint_t *fast;
     if (list == NULL || list->next == NULL) {
         return 0;
     }
 
-    listint_t *slow = list;
-    listint_t *fast = list->next;
+    slow = list;
+    fast = list->next;
 
     while (fast != NULL && fast->next != NULL) {
         if (slow == fast) {
