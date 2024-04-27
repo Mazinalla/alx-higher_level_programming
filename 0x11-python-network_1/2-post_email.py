36e8154f090841e2d9e43b9498a40984a27190e3
mazinalla <mazinelsadig8@gmail.com>
2024-04-27 05:34:22 +0300

    Net 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+import urllib.request
+import urllib.parse
+import sys
+
+# Get the URL and email from command-line arguments
+url = sys.argv[1]
+email = sys.argv[2]
+
+# Encode the email parameter
+data = urllib.parse.urlencode({'email': email})
+data = data.encode('utf-8')  # Encode the data to bytes
+
+# Send a POST request to the URL with the email as a parameter
+with urllib.request.urlopen(url, data) as response:
+    # Read and decode the body of the response
+    body = response.read().decode('utf-8')
+    
+    # Display the body of the response
+    print(body)
