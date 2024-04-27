791a00dfc0b49c4ebe557a8fad9f263e89e26020
mazinalla <mazinelsadig8@gmail.com>
2024-04-27 05:43:17 +0300

    Net 2

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""A script that:
+- takes in a URL,
+- sends a request to the URL and displays the value
+- of the X-Request-Id variable found in the header ofthe response.
+"""
 import urllib.request
 import sys
 
 # Get the URL from command-line arguments
-url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
 # Send a request to the URL
 with urllib.request.urlopen(url) as response:
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+"""A script that:
+- takes in a URL
+- sends a POST request to the passed URL
+- takes email as a parameter
+- displays the body of the response
+"""
 import urllib.request
 import urllib.parse
 import sys
 
 # Get the URL and email from command-line arguments
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
 # Encode the email parameter
 data = urllib.parse.urlencode({'email': email})
