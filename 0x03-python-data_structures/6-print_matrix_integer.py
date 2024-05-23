50dd1fe9e4ce38ccc4706caa89a5e37e0e121ed3
mazinalla <mazinelsadig8@gmail.com>
2024-05-23 07:56:52 -0700

    update some method

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        # Create a formatted string for each row, using str.format() for each element
         print(" ".join("{:d}".format(element) for element in row))
