bad5d21140c235c7705b2d065e20c54cad16dab3
mazinalla <mazinelsadig8@gmail.com>
2024-05-26 11:57:34 -0700

    Upgrade my code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,10 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     try:
         num_el = 0
-        for i in range(len(my_list)):
-            print(my_list[i], end="")
+        for i in my_list:
+            print(i, end="")
             num_el += 1
-            if i + 1 == x:
+            if (my_list.index(i) + 1) == x:
                 break
         print()
         return num_el
