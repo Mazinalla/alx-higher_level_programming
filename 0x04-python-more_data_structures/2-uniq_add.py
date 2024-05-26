8bf5744437a2a2a1cfbc2a6243850d30f521a7c9
mazinalla <mazinelsadig8@gmail.com>
2024-05-25 20:58:04 -0700

    update my previos code

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    '''function that adds all unique integers in a list (only once for each integer)'''
+    '''function that adds all unique integers in a list'''
     new_list = set(my_list)
     result = sum(new_list)
     return result
