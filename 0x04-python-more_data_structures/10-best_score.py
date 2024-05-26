edc813dc07728096316054d44f867adaba9c0f93
mazinalla <mazinelsadig8@gmail.com>
2024-05-25 22:26:09 -0700

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,5 +4,10 @@ def best_score(a_dictionary):
     '''Check if the dictionary is empty'''
     if not a_dictionary:
         return None
-    max_key = max(a_dictionary)
+    max_key = None
+    max_value = float('-inf')
+    for key, value in a_dictionary.items():
+        if value > max_value:
+            max_value = value
+            max_key = key
     return max_key
