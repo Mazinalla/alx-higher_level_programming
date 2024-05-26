6f919f031ba3be6f6e7166047ad7a4c785c27491
mazinalla <mazinelsadig8@gmail.com>
2024-05-25 23:44:18 -0700

    upgrade my code

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,7 @@ def roman_to_int(roman_string):
     result = 0
     prev_value = 0
     for char in reversed(roman_string):
-        value = roman_dict[char]
+        value = rom_di[char]
         if value < prev_value:
             result -= value
         else:
