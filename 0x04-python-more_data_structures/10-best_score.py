cb45c09f47189705d2fb5c151052fd59107d57f6
mazinalla <mazinelsadig8@gmail.com>
2024-05-25 22:23:17 -0700

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,12 +4,5 @@ def best_score(a_dictionary):
     '''Check if the dictionary is empty'''
     if not a_dictionary:
         return None
-
-    # Find the key with the maximum value
     max_key = max(a_dictionary)
-    
     return max_key
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
