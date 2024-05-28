e24004aafbbd6edd5b3e586cff47a7a05304a961
mazinalla <mazinelsadig8@gmail.com>
2024-05-27 23:23:57 -0700

    add new method

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_function(fct, *args):
     try:
         result = fct(*args)
         return result
     except Exception as e:
-        print(f"Exception:{e}", file=sys.stderr)
+        print(f"Exception: {e}", file=sys.stderr)
         return None
