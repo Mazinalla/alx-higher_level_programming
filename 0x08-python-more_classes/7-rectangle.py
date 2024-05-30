193c5550a234a39e116b13d53718af3bc1d35c59
mazinalla <mazinelsadig8@gmail.com>
2024-05-30 18:41:37 +0300

    add attr

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -11,7 +11,8 @@ class Rectangle:
     Class Rectangle that defines a rectangle.
     '''
     number_of_instances = 0
-    print_symbol = "#"
+    print_symbol = '#'
+
     def __init__(self, width=0, height=0):
         '''
         Method for instantiation with optional parameters:
