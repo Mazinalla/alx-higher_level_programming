7f0ea682b08e7ab137f22f3b4525832dd8455ad0
mazinalla <mazinelsadig8@gmail.com>
2024-06-07 00:26:48 +0300

    new

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,35 +6,7 @@ add two integer and return the value
 
 def add_integer(a, b=98):
     '''
-    Adds two integers or floats, converting floats to integers before adding.
-
-    Args:
-    a: The first number (int or float).
-    b: The second number (int or float), defaults to 98.
-
-    Returns:
-    The sum of a and b as an integer.
-
-    Raises:
-    TypeError: If either a or b are not integers or floats.
-
-    Examples:
-    >>> add_integer(1, 2)
-    3
-    >>> add_integer(100, -2)
-    98
-    >>> add_integer(2)
-    100
-    >>> add_integer(100.3, -2)
-    98
-    >>> add_integer(4, "School")
-    Traceback (most recent call last):
-        ...
-    TypeError: b must be an integer
-    >>> add_integer(None)
-    Traceback (most recent call last):
-        ...
-    TypeError: a must be an integer
+    function that add two integer
     '''
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+'''
+module with a function that divide all elem of a matrix
+'''
+
+
+def matrix_divided(matrix, div):
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    if not all(isinstance(elem, (int, float)) for row in matrix for elem in row):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    row_length = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_length:
+            raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return [[round(elem / div, 2) for elem in row] for row in matrix]
