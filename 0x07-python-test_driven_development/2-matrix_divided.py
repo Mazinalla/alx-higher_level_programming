8e1f7d4f415903e1933e4f79c1887ccf0780bcb7
mazinalla <mazinelsadig8@gmail.com>
2024-06-07 00:30:53 +0300

    new

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,11 +8,15 @@ def matrix_divided(matrix, div):
     '''
     function that divide all elem of a matrix
     '''
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list) or not all(isinstance(row, list)
+        for row in matrix):
+        raise TypeError
+    ("matrix must be a matrix (list of lists) of integers/floats")
 
-    if not all(isinstance(elem, (int, float)) for row in matrix for elem in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(elem, (int, float)) 
+        for row in matrix for elem in row):
+        raise TypeError
+    ("matrix must be a matrix (list of lists) of integers/floats")
 
     row_length = len(matrix[0])
     for row in matrix:
