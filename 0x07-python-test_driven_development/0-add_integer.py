341dcb5c7685a8a8d97a9dc7fa7ff75544d726ef
mazinalla <mazinelsadig8@gmail.com>
2024-06-06 23:07:39 +0300

    test my code

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-
+"""
+add two integer and return the value
+"""
 
 def add_integer(a, b=98):
+    '''
+    function that add two integer
+    '''
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     elif not isinstance(b, int) and not isinstance(b, float):
