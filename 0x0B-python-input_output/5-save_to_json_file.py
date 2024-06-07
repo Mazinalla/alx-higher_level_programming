74fb3800b2431c87cc197751b591a8c51954830c
mazinalla <mazinelsadig8@gmail.com>
2024-06-08 02:00:33 +0300

    seri and deseri

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """ Writes an object to a text file using a JSON representation """
     with open(filename, mode='w', encoding='utf-8') as file:
-        json.dump(my_obj, file, ensure_ascii=False, indent=2)
+        json.dump(my_obj, file)
