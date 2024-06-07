67aecab6a9dfcaf8b4b926072a9760d6540cc0c1
mazinalla <mazinelsadig8@gmail.com>
2024-06-07 08:38:42 +0300

    new meth

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -15,6 +15,7 @@ def say_my_name(first_name, last_name=""):
     return:
         My name is <first name> <last name>
     '''
+
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
