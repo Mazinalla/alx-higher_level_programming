2609dd7c371fcec3ed52994024d1cf7988b63a45
mazinalla <mazinelsadig8@gmail.com>
2024-06-11 18:16:25 +0300

    Orm

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ def list_states(username, password, dbname):
     '''
     method that use for connect with database and make some queris
     '''
-    # Connect to the database
+
     db = MySQLdb.connect(
         host="localhost",
         user=username,
@@ -19,28 +19,25 @@ def list_states(username, password, dbname):
         port=3306
     )
 
-    # Create a cursor object
+
     cursor = db.cursor()
 
-    # Execute the query to retrieve all states sorted by states.id
+
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Fetch all the rows
+
     states = cursor.fetchall()
 
-    # Print each state
+
     for state in states:
         print(state)
 
-    # Close the cursor and database connection
+
     cursor.close()
     db.close()
 
 if __name__ == "__main__":
-    # Retrieve command-line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     dbname = sys.argv[3]
-
-    # List all states
     list_states(username, password, dbname)
