488cb2f2a0cdf0e2ead770d25010d146bd0b9f3c
mazinalla <mazinelsadig8@gmail.com>
2024-06-11 18:21:30 +0300

    Orm

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+Script that lists all states with a name starting with 'N' from the database hbtn_0e_0_usa.
+
+Arguments:
+    mysql_username: MySQL username
+    mysql_password: MySQL password
+    database_name: MySQL database name
+
+The script connects to a MySQL server running on localhost at port 3306,
+and retrieves states whose names start with 'N', sorted by states.id in ascending order.
+"""
+import MySQLdb
+import sys
+
+
+
+def list_states_with_n(username, password, dbname):
+    '''
+    function that get arguments from user to connect to database and make queris
+    '''
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=dbname,
+        port=3306
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    list_states_with_n(username, password, dbname)
