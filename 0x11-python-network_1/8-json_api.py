e9ed6bdf7ecf194cb38894dd2c1a7d3d48840f89
mazinalla <mazinelsadig8@gmail.com>
2024-08-12 16:57:32 -0700

    another one

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+"""
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if __name__ == "__main__":
+        q = sys.argv[1] if len(sys.argv) > 1 else ""
+        data = {'q': q}
+        response = requests.post('http://0.0.0.0:5000/search_user', data=data)
+
+        try:
+            json_resp = response.json()
+
+            if json_response:
+                print(f"[{json_resp.get('id')}] {json_resp.get('name')}")
+            else:
+                # If the JSON is empty, display "No result"
+                print("No result")
+        except ValueError:
+            # If JSON decoding fails, display "Not a valid JSON"
+            print("Not a valid JSON")
