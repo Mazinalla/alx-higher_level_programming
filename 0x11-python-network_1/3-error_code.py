85aadb52a0373cb113c3c05a51ba1a0d04ccc5a9
mazinalla <mazinelsadig8@gmail.com>
2024-08-12 14:04:13 -0700

    fetch alx content

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,12 +6,13 @@ and displays the body of the response (decoded in utf-8).
 import urllib.request
 import sys
 
-url = sys.argv[1]
+
 if __name__ == "__main__":
+    url = sys.argv[1]
     try:
         with urllib.request.urlopen(url) as resp:
             body = resp.read().decode('utf-8')
-
             print(body)
+            
     except urllib.error.HTTPError as e:
         print(f"Error code: {e.code}")
