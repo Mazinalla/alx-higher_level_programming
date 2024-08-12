4ede99a7fccb75858a9f0463ce0dc6139a7b08fe
mazinalla <mazinelsadig8@gmail.com>
2024-08-12 12:43:38 -0700

    fetch alx content

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
+import urllib.request
+import sys
+
 """A script that:
 - takes in a URL,
 - sends a request to the URL and displays the value
 - of the X-Request-Id variable found in the header ofthe response.
 """
-import urllib.request
-import sys
+
 if __name__ == "__main__":
-    '''
-    store url in the variable url
-    '''
     url = sys.argv[1]
 with urllib.request.urlopen(url) as response:
     x_request_id = response.getheader('X-Request-Id')
