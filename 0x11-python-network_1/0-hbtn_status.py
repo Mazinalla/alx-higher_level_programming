b7246ea229cc52b3a5e3911ee363d5942ab381ec
mazinalla <mazinelsadig8@gmail.com>
2024-08-12 12:12:11 -0700

    fetch alx content

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,5 +4,6 @@ url = 'https://alx-intranet.hbtn.io/status'
 with urllib.request.urlopen(url) as response:
     body = response.read().decode('utf-8')
     print("Body response:")
-    print("\t- type:", type(body))
-    print("\t- content:", body)
+    print("\t- type:", type(response.read()))
+    print("\t- content:", response.read())
+    print("\t- utf8 content:", body)
