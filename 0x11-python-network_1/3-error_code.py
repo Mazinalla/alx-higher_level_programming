ca5923829656bee0f70304b0dd435b184fff739e
mazinalla <mazinelsadig8@gmail.com>
2024-08-12 14:00:36 -0700

    fetch alx content

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,15 +2,15 @@
 """
 
 """
-import urllib.requests
+import urllib.request
 import sys
 
 url = sys.argv[1]
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(url) as resp:
+            body = resp.read().decode('utf-8')
 
-try:
-    with requests.urlopen(url) as resp:
-    body = resp.read()
-
-    print(body)
-except urllib.error.HTTPError as e:
-    print(f"Error code: {e.code}")
+            print(body)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
