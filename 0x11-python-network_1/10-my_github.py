9f7bdb3a86109bd3871f54ba323289a7bb60d42f
mazinalla <mazinelsadig8@gmail.com>
2024-08-12 21:27:15 -0700

    access github API using my credentials

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Python script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+
+import requests
+import sys
+
+
+def get_github_user_id(username, token):
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(username, token))
+
+    if response.status_code == 200:
+        user_info = response.json()
+        print(f"Your GitHub ID is: {user_info['id']}")
+    else:
+        print(f"Failed to retr GitHub ID. Stat code: {response.status_code}")
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+    get_github_user_id(username, token)
